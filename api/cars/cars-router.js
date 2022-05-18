b6e62441a3d3b57cd3119b753d74fef206c7fe1c
devantequick <devantequick94@gmail.com>
2022-05-18 17:56:30 -0400

    mvp

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,38 @@
-// DO YOUR MAGIC
+const Car = require('./cars-model');
+const router = require('express').Router();
+const { checkCarPayload, checkVinNumberUnique, checkVinNumberValid } = require('./cars-middleware');
+
+router.get('/', (req, res) => {
+    Car.getAll()
+        .then(car => {
+            res.json(car);
+        })
+        .catch(() => {
+            res.status(500).json({ message: '500 error getAll' });
+        })
+})
+
+router.get('/:id', (req, res) => {
+    Car.getById(req.params.id)
+        .then(car => {
+            if (car) {
+                res.json(car);
+            } else {
+                res.status(404).json({ message: 'Failed to retrieve car' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: '500 error getById' });
+        });
+});
+
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res, next) => {
+    try {
+        const data = await Car.create(req.body);
+        res.status(201).json(data);
+    } catch(err) {
+        next(err)
+    }
+});
+  
+  module.exports = router;
